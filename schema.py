696a96e9b239fa5cc68a2c5eacc990a16164a4b5
ram <ramairline@gmail.com>
2026-01-16 09:18:56 -0600

    updated code

--- schema.py
+++ schema.py
@@ -1,14 +1,12 @@
-from pydantic import BaseModel, Field
-from typing import List
+from pydantic import BaseModel, Field # Import classes from Pydantic library to define, format, and validate data
+from typing import List # Import List class from Typing package
 
-
-class Source(BaseModel):
+class Source(BaseModel): # This class is designed to represent a single reference link used by the agent.
     """Schema for a source used by an agent"""
     url:str = Field(description="URL of the source")
 
-class AgentResponse(BaseModel):
+class AgentResponse(BaseModel): # This class structures the "final answer" from a research agent, breaking it down into specific technical categories
     """Schema for agent response with answer and sources"""
-
     value_proposition_analysis:str = Field(description="A summary of what the company claims vs what their tech supports")
     technical:str = Field(description="Is it proprietary code, an unique dataset, or wrapper?")
     developer_sentiment:str = Field(description="What the engineering community states about their tech?")
